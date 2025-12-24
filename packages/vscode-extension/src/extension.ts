ac0809d3351de31edd891ee06c8746ad39aedadf
Alade Yessoufou <aladecharaf23@gmail.com>
2025-12-24 21:15:19 +0100

    feat(vscode-extension): support popular template engine extension

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -1,19 +1,40 @@
-import * as vscode from 'vscode';
-import { TrivuleCompletionProvider } from './completionProvider';
+import * as vscode from "vscode";
+import { TrivuleCompletionProvider } from "./completionProvider";
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Trivule VS Code Extension is now active!');
+  const supportedLanguages = [
+    "html",
+    "php",
+    "blade",
+    "vue",
+    "svelte",
+    "astro",
+    "ejs",
+    "handlebars",
+    "mustache",
+    "nunjucks",
+    "pug",
+    "twig",
+    "erb",
+    "edge",
+    "eex",
+    "jinja",
+    "gotemplate",
+  ];
 
+  supportedLanguages.forEach((lang) => {
     const provider = vscode.languages.registerCompletionItemProvider(
-        'html',
-        new TrivuleCompletionProvider(),
-        ':', // Trigger for @v:
-        '.', // Trigger for @v:msg.
-        '|', // Trigger for chaining rules
-        '"'  // Trigger for starting attribute value
+      lang,
+      new TrivuleCompletionProvider(),
+      ":", // Trigger for @v:
+      ".", // Trigger for @v:msg.
+      "|", // Trigger for chaining rules
+      '"', // Trigger for starting attribute value
+      "'", // Trigger for starting attribute value
     );
 
     context.subscriptions.push(provider);
+  });
 }
 
-export function deactivate() { }
+export function deactivate() {}
